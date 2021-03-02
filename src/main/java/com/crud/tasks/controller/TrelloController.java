5957d87673f9edf1a947c03d421970c3c12e9311
Piotr Dziedzic <piotr.dziedzic1729@outlook.com>
2021-03-02 18:01:04 +0100

    Adding trello client

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -22,8 +22,16 @@ public class TrelloController {
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
         trelloBoards.stream()
-                .filter(s-> s.getName()!=null && s.getName()!=null && s.checkingWordInName("Kodilla"))
+                .filter(s-> s.getName()!=null && s.getId()!=null && s.getName().contains("Kodilla"))
                 .forEach(System.out::println);
+
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
+            System.out.println("This board contains lists: ");
+            trelloBoardDto.getLists().forEach(trelloList -> {
+                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
+            });
+        });
 
     }
 }
