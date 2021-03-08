975d40d58e2c4a0df1a4e336f7ace273e9fccc10
Piotr Dziedzic <piotr.dziedzic1729@outlook.com>
2021-03-08 01:35:38 +0100

    task 23.4

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -32,7 +32,7 @@ public class SimpleEmailService {
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
         if(mail.getToCc().isPresent()) {
-            mailMessage.setCc(mail.getToCc());
+            //mailMessage.setCc(mail.getToCc());
         }
         return mailMessage;
     }
