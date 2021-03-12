0cfc7af4531e7899da0682bb012153bfbed7b20c
Piotr Dziedzic <piotr.dziedzic1729@outlook.com>
2021-03-12 13:58:36 +0100

    Task controler tests

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -4,6 +4,7 @@ import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
+import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -30,11 +31,10 @@ class TaskControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    DbService dbService;
+    private DbService dbService;
 
     @MockBean
-    TaskMapper taskMapper;
-
+    private TaskMapper taskMapper;
 
     @Test
     public void shouldFetchListOfTasks() throws Exception {
@@ -51,5 +51,31 @@ class TaskControllerTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(1)));
     }
+
+    @Test
+    public void shouldFetchTask() throws Exception {
+
+        //Given
+        Task task = new Task(1L,"First title","First Description");
+        when(dbService.getTask(1L)).thenReturn(task);
+    }
+    @Test
+    public void shouldCreateTask() throws Exception {
+
+        //Given
+        TaskDto taskDto = new TaskDto(1L,"Random title","Random content");
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(taskDto);
+
+        //When & Then
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .get("/v1/task/createTask")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content(jsonContent))
+
+    }
+
 
 }
